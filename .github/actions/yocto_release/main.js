04197a9b033fe81aea0117ea42804cc335e3ec98
Hendrik <git@h3ndrk.de>
2022-12-26 00:10:34 +0100

    Checkout repositories with kas

--- .github/actions/yocto_release/main.js
+++ .github/actions/yocto_release/main.js
@@ -5,7 +5,7 @@ const github = require("@actions/github");
 
 const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
 const octokit = github.getOctokit(GITHUB_TOKEN);
-const distroConfPath = "conf/distro/HULKs-OS.conf";
+const distroConfPath = `${github.context.repo.repo}/conf/distro/HULKs-OS.conf`;
 
 async function release() {
   const distroVersion = readDistroVersion();
@@ -38,22 +38,67 @@ async function releaseImage(major, minor, patch) {
   //   );
   //   return;
   // }
-  await exec.exec("ls -ahl /");
-  await createRelease(`test`, "Test2", "Test2");
+  // await exec.exec("ls -ahl /");
+  // await exec.exec("pwd");
+  // await exec.exec("kas --help");
+  const tag = `image-${major}.${minor}.${patch}`;
+  const releaseId = await createRelease(
+    tag,
+    `HULKs-OS Image ${major}.${minor}.${patch}`,
+    `Flashable image containing HULKs-OS ${major}.${minor}.${patch}, for instructions see https://hulks.de/hulk/setup/overview/`
+  );
+  if (releaseId === null) {
+    core.info(
+      `Tag ${tag} already exists, will not release a new image for ${major}.${minor}.${patch}.`
+    );
+    return;
+  }
+  try {
+    await exec.exec("kas checkout meta-hulks/kas-project.yml");
+    await exec.exec("mkdir -p meta-nao/recipes-support/aldebaran/aldebaran-binaries");
+    await exec.exec("cp /aldebaran_binaries.tar.gz meta-nao/recipes-support/aldebaran/aldebaran-binaries/");
+  } catch (error) {
+    deleteRelease(releaseId);
+    throw error;
+  }
 }
 
 async function createRelease(tag, name, body) {
-  console.log(await octokit.request("POST /repos/{owner}/{repo}/releases", {
+  try {
+    const response = await octokit.request(
+      "POST /repos/{owner}/{repo}/releases",
+      {
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        tag_name: tag,
+        target_commitish: github.context.sha,
+        name,
+        body,
+        draft: false,
+        prerelease: false,
+        generate_release_notes: false,
+      }
+    );
+    return response.data.id;
+  } catch (error) {
+    if (
+      error.status === 422 &&
+      error.response.data.errors.some(
+        (error) => error.code === "already_exists" && error.field === "tag_name"
+      )
+    ) {
+      return null;
+    }
+    throw error;
+  }
+}
+
+async function deleteRelease(releaseId) {
+  await octokit.request("DELETE /repos/{owner}/{repo}/releases/{release_id}", {
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
-    tag_name: tag,
-    target_commitish: github.context.sha,
-    name,
-    body,
-    draft: false,
-    prerelease: false,
-    generate_release_notes: false,
-  }));
+    release_id: releaseId,
+  });
 }
 
 async function tagExists(tag) {
